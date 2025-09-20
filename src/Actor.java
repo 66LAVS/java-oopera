1e0200d26f1bfb0a8f093309c8f439e6ca36e880
Vladimir Ivanov <ivanov.vladimir.l@gmail.com>
2025-09-20 15:36:52 +0300

    init

---
+++ src/Actor.java
@@ -0,0 +1,8 @@
+public class Actor extends Person{
+    private double height;
+
+    public Actor(String name, String surname, Gender gender, double height) {
+        super(name, surname, gender);
+        this.height = height;
+    }
+}
---
+++ src/Show.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Show {
+    private String title;
+    private int duration;
+    private Director director;
+    private ArrayList<Actor> listOfActors;
+
+}
